d6b97c179d8c6f1b541872e214886bb9f8e91c09
Sergey Linok <serega.linok5555@yandex.ru>
2022-12-27 08:12:47 -0500

    update

--- src/state_machine/scripts/third_floor_to_first_floor.py
+++ src/state_machine/scripts/third_floor_to_first_floor.py
@@ -12,9 +12,9 @@ import communication_msgs.msg as Action
 import communication_msgs.srv as Service
 
 
-metadata = {"third_floor_lift_coord": {"x": -10, "y": -20},
+metadata = {"third_floor_lift_coord": {"x": 12, "y": 20.8},
             "322_door_type": "right",
-            "322_door_action": "pull",
+            "322_door_action": "push",
             "button_sign_in_lift_hall": 'down',
             "button_position_in_lift_hall": 0,
             "floor_sign_in_lift": '1',
@@ -41,28 +41,19 @@ def main():
 
     # Open the container
     with state_machine:
-        # Add states to the container
-        smach.StateMachine.add('move_to_point_ACTION_1',
-                                SimpleActionState('move_to_point',
-                                    Action.MoveToPointAction,
-                                    goal=Action.MoveToPointActionGoal(**metadata.get("third_floor_lift_coord")),
-                                    result_slots=["x1", "y1", "x2", "y2"]),
-                                transitions={'succeeded': 'door_coord_ACTION_1',
-                                             'preempted': 'finish',
-                                             'aborted': 'finish'})
 
         smach.StateMachine.add('door_coord_ACTION_1',
-                                SimpleActionState('door_coord',
-                                    Action.DoorCoordAction,
-                                    goal_slots=["x1", "y1", "x2", "y2"]),
-                                transitions={'succeeded': 'navigation_ACTION_1',
-                                             'preempted': 'finish',
-                                             'aborted': 'finish'})
+                        SimpleActionState('door_coord',
+                            Action.DoorCoordAction,
+                            goal=Action.DoorCoordGoal(x1=-0.95, y1=2.37, x2=-0.08, y2=2.775)),   
+                        transitions={'succeeded': 'navigation_ACTION_2',
+                                        'preempted': 'finish',
+                                        'aborted': 'finish'})
 
         smach.StateMachine.add('navigation_ACTION_1',
                                 SimpleActionState('navigate_2d',
                                     Action.Navigate2DAction,
-                                    goal=Action.Navigate2DGoal(goal="makeparking")),
+                                    goal=Action.Navigate2DGoal(goal="parkingin")),
                                 transitions={'succeeded': 'open_the_door_SERVICE',
                                              'preempted': 'finish',
                                              'aborted': 'finish'})
@@ -72,10 +63,10 @@ def main():
                                     Service.OpenDoor,
                                     request = Service.OpenDoorRequest(
                                         door_type=metadata.get("322_door_type"),
-                                        action_type=metadata.get("322_door_action")),
+                                        action_type=metadata.get("322_door_action"))),
                                 transitions={'succeeded': 'navigation_ACTION_2',
                                              'preempted': 'finish',
-                                             'aborted': 'finish'}))
+                                             'aborted': 'finish'})
 
         smach.StateMachine.add('navigation_ACTION_2',
                                 SimpleActionState('navigate_2d',
@@ -95,19 +86,29 @@ def main():
         smach.StateMachine.add('navigation_ACTION_3',
                                 SimpleActionState('navigate_2d',
                                     Action.Navigate2DAction,
-                                    goal=Action.Navigate2DGoal(goal="gotoroom")),
-                                transitions={'succeeded': 'move_to_point_ACTION',
+                                    goal=Action.Navigate2DGoal(goal="gotocoridour")),
+                                transitions={'succeeded': 'move_to_point_ACTION_1',
                                              'preempted': 'finish',
                                              'aborted': 'finish'})
 
-        smach.StateMachine.add('move_to_point_ACTION',
-                                SimpleActionState('move_to_point',
-                                    Action.MoveToPointAction,
-                                    goal=Action.MoveToPointActionGoal(**metadata.get("third_floor_lift_coord")),
-                                    result_slots=["x1", "y1", "x2", "y2"]),
-                                transitions={'succeeded': 'door_coord_ACTION_2',
-                                             'preempted': 'finish',
-                                             'aborted': 'finish'})
+        smach.StateMachine.add('move_to_point_ACTION_1',
+                                        SimpleActionState('move_to_point',
+                                            Action.MoveToPointAction,
+                                            goal=Action.MoveToPointGoal(x = 12 , y=20.8 ),
+                                            result_slots=["x1", "y1", "x2", "y2"]),
+                                        transitions={'succeeded': 'push_button_SERVICE',
+                                                    'preempted': 'finish',
+                                                    'aborted': 'move_to_point_ACTION_1'})
+
+        smach.StateMachine.add('push_button_SERVICE',
+                                        ServiceState('/elev/push_button',
+                                            Service.PushButton,
+                                            request = Service.PushButtonRequest(
+                                                lmr=metadata.get("near_elevator_sign_pos"),
+                                                symbol=metadata.get("down_sign_lift"))),
+                                        transitions={'succeeded': 'door_coord_ACTION_2',
+                                                    'preempted': 'finish',
+                                                    'aborted': 'finish'})
 
         smach.StateMachine.add('door_coord_ACTION_2',
                                 SimpleActionState('door_coord',
@@ -121,23 +122,19 @@ def main():
                                 SimpleActionState('navigate_2d',
                                     Action.Navigate2DAction,
                                     goal=Action.Navigate2DGoal(goal="liftparking")),
-                                transitions={'succeeded': 'push_button_SERVICE',
+                                transitions={'succeeded': 'push_button_SERVICE_2',
                                              'preempted': 'finish',
                                              'aborted': 'finish'})
 
-        smach.StateMachine.add('push_button_SERVICE',
+        smach.StateMachine.add('push_button_SERVICE_2',
                                 ServiceState('PushButton',
                                     Service.PushButton,
                                     request = Service.PushButtonRequest(
                                         lmr=metadata.get("button_position_in_lift_hall"),
                                         symbol=metadata.get("button_sign_in_lift_hall"))),
-                                transitions={'succeeded': 'Lift_door_bottleneck_1',
+                                transitions={'succeeded': 'navigation_ACTION_5',
                                              'preempted': 'finish',
                                              'aborted': 'finish'})
-
-        smach.StateMachine.add('Lift_door_bottleneck_1',
-                                Lift_door_bottleneck(),
-                                transitions={'lift_passing':'navigation_ACTION_5'})
 
         smach.StateMachine.add('navigation_ACTION_5',
                                 SimpleActionState('navigate_2d',
@@ -153,7 +150,7 @@ def main():
                                     request = Service.PushButtonRequest(
                                         lmr=metadata.get("floor_sign_position_in_lift"),
                                         symbol=metadata.get("floor_sign_in_lift"))),
-                                transitions={'succeeded': 'Lift_door_bottleneck_2',
+                                transitions={'succeeded': 'finish',
                                              'preempted': 'finish',
                                              'aborted': 'finish'})
 
