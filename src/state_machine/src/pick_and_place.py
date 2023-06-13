58688d0807269a98e563b10730422187ebb84d9d
Sergey <serega.linok5555@yandex.ru>
2023-06-13 13:01:15 -0400

    fix after experiment

--- src/state_machine/src/pick_and_place.py
+++ src/state_machine/src/pick_and_place.py
@@ -12,32 +12,23 @@ def main():
     state_machine = smach.StateMachine(outcomes=['finish'])
 
     with state_machine:
-        smach.StateMachine.add('move_to_point_ACTION_1',
-                                SimpleActionState('move_to_point_with_orientation',
-                                    Action.MoveToPointWithOrientationAction,
-                                    goal=Action.MoveToPointWithOrientationGoal(
-                                        x=-2.35, y=-6.85, theta=0, use_orientation=False)),
-                                    transitions={'succeeded': 'pick_object_SERVICE',
-                                                 'preempted': 'finish',
-                                                 'aborted': 'finish'})
-
         request = Service.PickObjectRequest(what="any_object", where="any_place")
         smach.StateMachine.add('pick_object_SERVICE',
                                 ServiceState('pick_object',
                                     Service.PickObject,
                                     request=request),
-                                transitions={'succeeded': 'move_to_point_ACTION_2',
+                                transitions={'succeeded': 'move_to_point_ACTION_1',
                                              'preempted': 'finish',
                                              'aborted': 'finish'})
 
-        smach.StateMachine.add('move_to_point_ACTION_2',
+        smach.StateMachine.add('move_to_point_ACTION_1',
                                 SimpleActionState('move_to_point_with_orientation',
                                     Action.MoveToPointWithOrientationAction,
                                     goal=Action.MoveToPointWithOrientationGoal(
-                                        x=0, y=0, theta=1.57, use_orientation=True)),
-                                transitions={'succeeded': 'place_object_SERVICE',
-                                             'preempted': 'finish',
-                                             'aborted': 'finish'})
+                                        x=42.7, y=-4.9, theta=0.7, use_orientation=True)),
+                                    transitions={'succeeded': 'place_object_SERVICE',
+                                                 'preempted': 'finish',
+                                                 'aborted': 'finish'})
         
         request = Service.PlaceObjectRequest(where="any_place")
         smach.StateMachine.add('place_object_SERVICE',
